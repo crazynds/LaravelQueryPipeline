5910e533715ce9158926c5056a60de23c3a2e22d
Crazynds <luli.broch@gmail.com>
2022-12-21 10:15:35 -0300

    feat: order by agora possibilita ordenar por multiplas colunas

--- src/Middleware/OrderByQuery.php
+++ src/Middleware/OrderByQuery.php
@@ -2,24 +2,30 @@
 
 namespace Crazynds\QueryPipeline\Middleware;
 
+use Crazynds\QueryPipeline\Middleware\QueryMiddleware;
+
 class OrderByQuery extends QueryMiddleware
 {
     protected function apply($query, array $data, $params)
     {
         $def = true;
-        if (isset($data['sortBy'])) {
-            $desc = ($data['sortDesc'] ?? false) ? 'desc' : 'asc';
+        $desc = ($data['sortDesc'] ?? false) ? 'desc' : 'asc';
+        if(isset($data['sortBy'])){
             $column = $data['sortBy'];
             foreach ($params as $name => $columns) {
                 $tablename = $this->getTableName($name);
-                if (in_array($column, $columns)) {
-                    $query->orderBy($tablename.'.'.$column, $desc);
+                if(in_array($column,$columns)){
+                    $query->orderBy($tablename.'.'.$column,$desc);
                     $def = false;
                 }
             }
         }
-        if ($def && isset($params['default'])) {
-            $query->orderBy($params['default']);
+        if($def && isset($params['default'])){
+            if(is_array($params['default'])){
+                foreach($params['default'] as $order){
+                    $query->orderBy($order, $desc);
+                }
+            }else $query->orderBy($params['default'], $desc);
         }
     }
 }
