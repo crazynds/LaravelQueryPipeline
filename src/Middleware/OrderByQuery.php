804994307a20debe30c0709f223384507dee27d2
crazynds <crazynds@users.noreply.github.com>
2022-12-21 13:16:04 +0000

    Fix styling

--- src/Middleware/OrderByQuery.php
+++ src/Middleware/OrderByQuery.php
@@ -2,30 +2,30 @@
 
 namespace Crazynds\QueryPipeline\Middleware;
 
-use Crazynds\QueryPipeline\Middleware\QueryMiddleware;
-
 class OrderByQuery extends QueryMiddleware
 {
     protected function apply($query, array $data, $params)
     {
         $def = true;
         $desc = ($data['sortDesc'] ?? false) ? 'desc' : 'asc';
-        if(isset($data['sortBy'])){
+        if (isset($data['sortBy'])) {
             $column = $data['sortBy'];
             foreach ($params as $name => $columns) {
                 $tablename = $this->getTableName($name);
-                if(in_array($column,$columns)){
-                    $query->orderBy($tablename.'.'.$column,$desc);
+                if (in_array($column, $columns)) {
+                    $query->orderBy($tablename.'.'.$column, $desc);
                     $def = false;
                 }
             }
         }
-        if($def && isset($params['default'])){
-            if(is_array($params['default'])){
-                foreach($params['default'] as $order){
+        if ($def && isset($params['default'])) {
+            if (is_array($params['default'])) {
+                foreach ($params['default'] as $order) {
                     $query->orderBy($order, $desc);
                 }
-            }else $query->orderBy($params['default'], $desc);
+            } else {
+                $query->orderBy($params['default'], $desc);
+            }
         }
     }
 }
